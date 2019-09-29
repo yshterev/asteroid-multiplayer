3a7a19679b8592591a38a925175725d12f206541
Yanko Shterev <yshterev@gmail.com>
2019-09-29 17:57:59 +0300

    [WIP] Sockets almost working properly...

--- src/client/actors/player/player.class.ts
+++ src/client/actors/player/player.class.ts
@@ -1,64 +1,42 @@
-import { KeyBoardControl } from "../../controls/keyboard.class";
-import { Projectile } from "../../props/powers/projectile/projectile.class";
-import { Hud } from "../../hud/hud.class";
-import Phaser from "phaser-ce";
-
-export interface IPlayer extends Phaser.Sprite {
-  id: string;
-}
+import {KeyBoardControl} from '../../controls/keyboard.class';
+import {Projectile} from '../../props/powers/projectile/projectile.class';
+import {Hud} from '../../hud/hud.class';
 
 export class Player {
-  public player: IPlayer;
+  public player: Phaser.Sprite;
+  public projectile: Projectile;
   public controls: KeyBoardControl;
   public playerState: Map<string, boolean | number>;
-  public angularVelocity: number = 600;
-  public projectile: Projectile;
   public hud: Hud;
+  public angularVelocity: number = 300;
 
-  constructor(private gameInstance: Phaser.Game, public playerInstance?: any) {
+  constructor(private gameInstance: Phaser.Game, public playerInstance: any) {
     this.createPlayer(this.gameInstance);
     this.playerState = new Map();
   }
 
-  public createPlayer(gameInstance: Phaser.Game): void {
+  public createPlayer(gameInstance): void {
     this.hud = new Hud();
-
     this.addControls();
-    this.player = gameInstance.add.sprite(
-      100,
-      100,
-      "shooter-sprite"
-    ) as IPlayer;
-    this.player.id = "1";
+    this.player = gameInstance.add.sprite(this.playerInstance.x, this.playerInstance.y, 'shooter-sprite');
+    this.player.id = this.playerInstance.id;
     this.player.anchor.setTo(0.5, 0.5);
-    this.player.animations.add("accelerating", [1, 0], 60, false);
-    this.player.name = "Your name";
+    this.player.animations.add('accelerating', [1, 0], 60, false);
+    this.player.name = this.playerInstance.name;
     this.attachPhysics(gameInstance);
-
     this.hud.setName(gameInstance, this.player);
   }
 
   public assignPickup(game, player?): void {
-    // create a new instance of the projectile and assign it
-    // immediately to the player who has picked up the projectile
     this.projectile = new Projectile(game, player.player);
-
-    // update the player state indicating that the player has ammo in
-    // her possession, let's set the ammo to the projectile's default
-    // bullet count to make the source of truth at one place
-    this.playerState.set("ammo", this.projectile.bulletCount);
-
-    // update hud on ammo pickup
     this.hud.setAmmo(game, player.player, this.projectile);
+    this.playerState.set('ammo', this.projectile.bulletCount);
   }
 
   public view(): void {
     this.controls.update();
-
     if (this.projectile) {
-      // take advantage of the game-loop to update the ammo
-      // count if the player has been using her projectiles
-      this.hud.update(this.playerState.get("ammo"));
+      this.hud.update(this.playerState.get('ammo'));
     }
   }
 
@@ -71,8 +49,8 @@ export class Player {
     this.player.body.collideWorldBounds = true;
     this.player.body.bounce.setTo(10, 10);
     this.player.body.gravity.y = 0;
-    this.player.body.drag.set(150);
-    this.player.body.maxVelocity.set(200);
+    this.player.body.drag.set(80);
+    this.player.body.maxVelocity.set(100);
     this.player.body.immovable = false;
   }
 }
--- src/client/props/powers/projectile/projectile.class.ts
+++ src/client/props/powers/projectile/projectile.class.ts
@@ -1,14 +1,13 @@
 import { Pickup } from "../pickup/pickup.class";
+import { Explode } from "../../explosion/explosion.class";
 
 export class Projectile {
   public weapon: Phaser.Weapon;
   public bulletCount: number = 10;
   public pickup: Pickup;
   private player: Phaser.Sprite;
-  private gameInstance: Phaser.Game;
 
-  public constructor(gameInstance, player?) {
-    this.gameInstance = gameInstance;
+  public constructor(private gameInstance: Phaser.Game, player?) {
     this.weapon = this.gameInstance.add.weapon(10, "laser");
     this.weapon.fireLimit = this.bulletCount;
     this.weapon.fireRate = 150;
@@ -26,6 +25,12 @@ export class Projectile {
   }
 
   public renderPickup(coors): void {
-    this.pickup = new Pickup(this.gameInstance, { x: 12, y: 12 });
+    this.pickup = new Pickup(this.gameInstance, coors);
+  }
+
+  // our kaboom class is fairly short and straight forward
+  public kaboom(projectile) {
+    // all we need a new class instance with the following arguments
+    new Explode(this.gameInstance, projectile);
   }
 }
